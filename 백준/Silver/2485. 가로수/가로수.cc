17d865fed5accd8aa2b4291f232549028dffd1f6
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-02-02 20:43:25 +0900

    [Silver IV] Title: 가로수, Time: 60 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int Euclidean(int a, int b) {
+    int r = a % b;
+    if(r == 0)
+        return b;
+    else
+        return Euclidean(b, r);
+}
+
+int main() {
+    int n, tmp1, tmp2, gcd, result = 0;
+    vector<int> distance;
+    
+    cin >> n;
+    
+    cin >> tmp1;
+    
+    // get distances btw trees
+    for(int i = 0; i < n-1; i++) {
+        cin >> tmp2;
+        distance.push_back(tmp2-tmp1);
+        tmp1 = tmp2;
+    }
+    
+    // sort distances
+    sort(distance.rbegin(), distance.rend());
+    
+    // get gcd using Euclidean
+    gcd = distance[0];
+    for(int i = 1; i < n-1; i++) {
+        gcd = Euclidean(gcd, distance[i]);
+    }
+    
+    // get the result
+    for(int i = 0; i < n-1; i++) {
+        result += (distance[i] / gcd) - 1;
+    }
+    
+    cout << result;
+}
