9df54123fe1a531b64c9b10dc19e1ab9c6d2937a
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-02-17 16:56:52 +0900

    Create 15650. N과 M (2).cc

---
+++ Algorithm/Backtracking/15650. N과 M (2).cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int *result;
+
+bool promising(int i) {
+	int j = 0;
+	
+	while(j < i) {
+		if(result[i] <= result[j]) return false;
+		j++;
+	}
+	return true; 
+}  
+
+void backtracking(int node) {
+	if(node == M) {
+		for(int i = 0; i < M; i++) cout << result[i] << ' ';
+		cout << '\n';
+	} else {
+		for(int i = 1; i <= N; i++) {
+			result[node] = i;
+			if(promising(node)) backtracking(node+1);
+		}
+	}
+}
+
+int main() {
+    cin >> N >> M;
+    result = new int[M];
+    
+    backtracking(0);
+}
