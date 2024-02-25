43f44566814e883e4200f6a6f309381d8e962905
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-02-25 16:04:09 +0900

    Create 1904. 01타일.cc

---
+++ Algorithm/Dynamic Programming/1904. 01타일.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int array[1000001];
+
+int main() {
+    int n;
+    
+    cin >> n;
+    
+    array[0] = 1;
+    array[1] = 2;
+    
+    for(int i = 2; i < n; i++) {
+        array[i] = (array[i-1] + array[i-2]) % 15746;
+    }
+    
+    cout << array[n-1] % 15746;
+}
