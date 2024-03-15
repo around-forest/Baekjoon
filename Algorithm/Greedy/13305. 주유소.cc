375f0edd517ea93a0e50883b03aceda3355ee0c4
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-03-15 15:25:49 +0900

    Create 13305. 주유소.cc

---
+++ Algorithm/Greedy/13305. 주유소.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+long long n, minCost;
+long long dist[100001], cost[100001];
+long long totalCost = 0;
+
+int main() {
+    cin >> n;
+    
+    for(int i = 0; i < n-1; i++) {
+        cin >> dist[i];
+    }
+    
+    for(int i = 0; i < n; i++) {
+        cin >> cost[i];
+    }
+    
+    minCost = cost[0];
+    totalCost = cost[0] * dist[0];
+    
+    for(int i = 1; i < n-1; i++) {
+        if(cost[i] < minCost) {
+            minCost = cost[i];
+            totalCost += minCost * dist[i];
+        } else {
+            totalCost += minCost * dist[i];
+        }
+    }
+    
+    cout << totalCost;
+}
