fcdcf7649b95686059be19469d6a3272c04eb522
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-03-26 19:50:14 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+#include <cstdlib>
+using namespace std;
+
+int N, tmp;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    
+    while(N--) {
+        cin >> tmp;
+        
+        if(tmp == 0) {
+            if(heap.empty()) cout << 0 << '\n';
+            else {
+                cout << heap.top().second << '\n';
+                heap.pop();
+            }
+        } else {
+            heap.push(make_pair(abs(tmp), tmp));
+        }
+    }
+}
