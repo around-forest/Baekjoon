f26c15668e7a392678adf35711d52cb3b82c3356
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-04-02 22:47:32 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 496 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+vector<int> series, result;
+
+int binarySearch(int k) {
+    int low = 0, high = result.size() - 1, mid;
+    
+    while(low < high) {
+        mid = low + (high - low) / 2;
+        
+        if(result[mid] >= k) high = mid;
+        else low = mid + 1;
+    }
+    
+    return high;
+}
+
+int main() {
+    cin >> N;
+    
+    for(int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        series.push_back(tmp);
+    }
+    result.push_back(series.front());
+    
+    for(int i = 1; i < N; i++) {
+        int tmp, index;
+          
+        if(result.back() < series[i]) {
+            result.push_back(series[i]);
+        } else {
+            result[binarySearch(series[i])] = series[i];
+        }
+    }
+    
+    cout << result.size();
+}
