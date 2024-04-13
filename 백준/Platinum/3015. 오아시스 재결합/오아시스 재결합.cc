ded7daa9c0c105c6740c51946a50f3bb082cf7d0
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-04-13 23:09:59 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 324 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+long long N, result = 0;
+stack<pair<int, int>> line;
+
+int main() {
+    cin >> N;
+    
+    while(N--) {
+        int tmp;
+        int count = 1;
+        cin >> tmp;
+        
+        while(!line.empty() && line.top().first <= tmp) {
+            result += line.top().second;
+            
+            if(line.top().first == tmp) count += line.top().second;
+            line.pop();
+        }
+        
+        if(!line.empty()) result++;
+        
+        line.push(make_pair(tmp, count));
+    }
+    
+    cout << result;
+}
