101a3d8700fbc67d2d6cdb7b9cb1062988770d40
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-05-01 23:41:31 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, v, day = 1;
+    
+    cin >> a >> b >> v;
+    day += (v-a) / (a-b);
+    if((v-a) % (a-b) != 0) day++;
+    cout << day;
+}
