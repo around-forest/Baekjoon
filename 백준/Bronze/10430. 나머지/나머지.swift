f99ca3053fc0b1b5ba4b867d87d21666979cf9b6
Forest Lee <86788943+around-forest@users.noreply.github.com>
2024-06-24 19:33:04 +0900

    [Bronze V] Title: 나머지, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.swift
@@ -0,0 +1,9 @@
+let input = readLine()!.split(separator: " ")
+let A = Int(input[0])!
+let B = Int(input[1])!
+let C = Int(input[2])!
+
+print((A + B) % C)
+print(((A % C) + (B % C)) % C)
+print((A * B) % C)
+print(((A % C) * (B % C)) % C)
